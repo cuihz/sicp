53fd84d5d9880b84bcf95a7700218f7dc582a1de
cuihz-mac <cuihz@cuihz-macs-Mac.local>
2015-03-01 20:55:00 +0800

    add 3_226~27

--- Ex3_25.scm
+++ Ex3_25.scm
@@ -30,14 +30,58 @@
 	       (let ((subtable (assoc (car l) (cdr table))))
 		 (if subtable
 		     (iter-insert! (cdr l) subtable)
-		     (set-cdr! table
-			       (cons (list (car l)
-					   (iter-insert! (cdr l) subtable))
-				     (cdr table))))))))
-      (iter-insert! key-list local-table))
+		     (let ((sub-table (list (car l))))
+		       (begin (iter-insert! (cdr l) sub-table)
+			      (set-cdr! table
+					(cons sub-table
+					      (cdr table))))))))))
+         (if (null? (cdr table)) 
+             (begin (display "-no entries-") 
+                    (newline)) 
+             (for-each (lambda (record) 
+                         (print-record record level)) 
+                       (cdr table)))) 
+  
+       (print-table local-table 0))
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
 	    ((eq? m 'insert-proc!) insert!)
+	    ((eq? m 'print) print)
 	    (else (error "Unknown operation -- TABLE" m))))
     dispatch))
 
+(define op-table (make-table eq?))
+
+(define put (op-table 'insert-proc!))
+
+(define get (op-table 'lookup-proc))
+
+((op-table 'print))
+
+(put '(letter a) 97)
+
+(put '(letter b ) 98)
+
+(put '(math +) 43)
+
+(put '(math -) 45)
+
+(put '(math *) 42)
+
+(put '(greek majiscule Λ) 923)
+
+(put '(greek miniscule λ) 955)
+
+(put '(min) 42)
+
+(put '(max) 955)
+
+(get '(min))
+
+(get '(letter b))
+
+(get '(greek miniscule λ))
+
+(get '(dfs))
+
+((op-table 'print))
